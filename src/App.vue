ef87897d5d5f118fbf155526faf4e44f3cdc9143
Alexey <aaivanov23@gmail.com>
2019-06-22 14:00:07 +0300

    add files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-container>
+        <v-layout>
+            <v-flex>
+                <v-btn>Все задачи</v-btn>
+                <v-data-table
+                        :headers="headers"
+                        :items="myJson"
+                        class="elevation-1"
+                >
+                    <template v-slot:items="props">
+                        <td>{{ props.item.require_date }}</td>
+                        <td class="text-md-left">{{ props.item.executor }}</td>
+                        <td class="text-md-left">{{ props.item.type_id }}</td>
+                        <td class="text-md-left">{{ props.item.description }}</td>
+                        <td class="text-md-left">{{ props.item.date_create }}</td>
+                    </template>
+                </v-data-table>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import json from '../data.json';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        data() {
+            return {
+                headers: [
+                    {text: 'Срок', align: 'left', sortable: false},
+                    {text: 'Исполнитель', align: 'left', sortable: false},
+                    {text: 'Тип', align: 'left', sortable: false},
+                    {text: 'Краткое содержание', align: 'left', sortable: false},
+                    {text: 'Дата создания задачи', align: 'left', sortable: false},
+                ],
+                myJson: json,
+            }
+        },
+        methods: {
+        }
+    }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
